a54af96dc6f9aa85960ac33e217cd7d853f65bc6
Dip2K <hjkim@geoservice.co.kr>
2024-05-01 15:58:58 +0900

    재질 변경

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,18 @@
-import { DEG2RAD } from 'three/src/math/MathUtils.js'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 import './style.css'
 import * as THREE from "three"
-import { OrbitControls } from 'three/examples/jsm/Addons.js'
-import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js'
 
 class App {
   private domApp: Element
   private renderer: THREE.WebGLRenderer
   private scene: THREE.Scene
   private camera?: THREE.PerspectiveCamera
-  private orbitControls?: OrbitControls;
 
   constructor() {
     console.log('Hello three.js')
 
     this.domApp = document.querySelector('#app')!
-    
+
     this.renderer = new THREE.WebGLRenderer({ antialias: true })
     this.renderer.setPixelRatio(window.devicePixelRatio)
     this.domApp.appendChild(this.renderer.domElement)
@@ -23,23 +20,19 @@ class App {
 
     this.setupCamera()
     this.setupLight()
-    this.setupControls()
     this.setupModels()
-    this.setupGUI()
     this.setupEvents()
-  }
-
-  private setupControls() {
-    this.orbitControls = new OrbitControls(this.camera!, this.domApp as HTMLElement)
   }
 
   private setupCamera() {
     const domApp = this.domApp
     const width = domApp.clientWidth
     const height = domApp.clientHeight
-    
-    this.camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 100)
-    this.camera.position.set(0, 2, 5);
+
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100)
+    this.camera.position.set(2,2,3.5);
+
+    new OrbitControls(this.camera, this.domApp as HTMLElement)
   }
 
   private setupLight() {
@@ -50,66 +43,49 @@ class App {
     this.scene.add(light)
   }
 
-  private update(time: number) {
-    time *= 0.001 // ms -> s    
-
-    this.orbitControls?.update();
-  }
-
-  private setupGUI() {
-    const gui = new GUI()
-        
-  }
-
-  private setupModels() {  
+  private setupModels() {
     const axisHelper = new THREE.AxesHelper(10)
     this.scene.add(axisHelper)
 
-    // Add Plane
-    const geomPlane = new THREE.PlaneGeometry(5, 5)
-    const matPlane = new THREE.MeshPhongMaterial()
-    const plane = new THREE.Mesh(geomPlane, matPlane)
-    plane.name = "plane"
-    plane.rotateX(-DEG2RAD*90)
-    plane.translateZ(-0.5);
-    this.scene.add(plane)
+    const geomGround = new THREE.PlaneGeometry(5, 5)
+    const matGround = new THREE.MeshStandardMaterial({ color: "#2c3e50", roughness: 0.5, metalness: 0.5, side: THREE.DoubleSide })
+    const ground = new THREE.Mesh(geomGround, matGround)
+    ground.rotation.x = -THREE.MathUtils.degToRad(90)
+    ground.position.y = -.5;
+    this.scene.add(ground);
 
-    // Add Big ball
-    const geomBigBall = new THREE.SphereGeometry(1, 32, 16, 0, DEG2RAD*360, 0, DEG2RAD*90);
-    const matBigBall = new THREE.MeshPhongMaterial();
-    const bigBall = new THREE.Mesh(geomBigBall, matBigBall);
-    bigBall.name = "bigBall"
-    bigBall.translateY(-0.5);
-    this.scene.add(bigBall)
+    const geomBigSphere = new THREE.SphereGeometry(1, 32, 16, 0,
+      THREE.MathUtils.degToRad(360), 0, THREE.MathUtils.degToRad(90));
+    const matBigSphere = new THREE.MeshStandardMaterial({ color: "#ffffff", roughness: 0.1, metalness: 0.2 });
+    const bigSphere = new THREE.Mesh(geomBigSphere, matBigSphere);
+    bigSphere.position.y = -0.5;
+    this.scene.add(bigSphere);
 
-    // Add 8 Toruses
+    const geomSmallSphere = new THREE.SphereGeometry(0.2);
+    const matSmallSphere = new THREE.MeshStandardMaterial({ color: "#e74c3c", roughness: 0.2, metalness: 0.5 });
+    const smallSphere = new THREE.Mesh(geomSmallSphere, matSmallSphere);
+
+    const smallSpherePivot = new THREE.Object3D();
+    smallSpherePivot.add(smallSphere);
+    bigSphere.add(smallSpherePivot);
+    smallSphere.position.x = 2;
+    smallSpherePivot.rotation.y = THREE.MathUtils.degToRad(-45)
+    smallSpherePivot.position.y = 0.5;
+    smallSpherePivot.name = "smallSpherePivot";
+
     const cntItems = 8;
     const geomTorus = new THREE.TorusGeometry(0.3, 0.1);
-    const matTorus = new THREE.MeshPhongMaterial();
-
+    const matTorus = new THREE.MeshStandardMaterial({ color: "#9b59b6", roughness: 0.5, metalness: 0.9 });
     for(let i=0; i<cntItems; i++) {
-      const torus = new THREE.Mesh(geomTorus, matTorus)
+      const torus = new THREE.Mesh(geomTorus, matTorus);
+      const torusPivot = new THREE.Object3D();
 
-      const pivotTorus = new THREE.Object3D()
-      this.scene.add(pivotTorus)
-  
-      torus.translateX(2)
-      pivotTorus.rotateY(DEG2RAD*360 / cntItems * i)
-
-      pivotTorus.add(torus)
-      torus.name = `torus-${i}`
+      bigSphere.add(torusPivot);
+      torus.position.x = 2;
+      torusPivot.position.y = 0.5;
+      torusPivot.rotation.y = THREE.MathUtils.degToRad(360) / cntItems * i;
+      torusPivot.add(torus);
     }
-
-    // Add Small ball
-    const geomSmallBall = new THREE.SphereGeometry(0.2);
-    const matSmallBall = new THREE.MeshPhongMaterial();
-    const smallBall = new THREE.Mesh(geomSmallBall, matSmallBall)
-    const pivotSmallBall = new THREE.Object3D()
-    
-    pivotSmallBall.add(smallBall)
-    smallBall.translateX(2)
-    smallBall.name = "smallBall"
-    this.scene.add(pivotSmallBall);
   }
 
   private setupEvents() {
@@ -124,12 +100,27 @@ class App {
     const height = domApp.clientHeight
 
     const camera = this.camera
-    if(camera) {
+    if (camera) {
       camera.aspect = width / height
       camera.updateProjectionMatrix()
     }
 
     this.renderer.setSize(width, height)
+  }
+
+  private update(time: number) {
+    time *= 0.001 // ms -> s
+
+    const smallSpherePivot = this.scene.getObjectByName("smallSpherePivot");
+    if(smallSpherePivot) {
+      // smallSpherePivot.rotation.y = time;   
+      const euler = new THREE.Euler(0, time, 0);
+      const quaterion = new THREE.Quaternion().setFromEuler(euler);
+      smallSpherePivot.setRotationFromQuaternion(quaterion);
+
+
+      //smallSpherePivot.quaternion.setFromEuler(euler);
+    }
   }
 
   private render(time: number) {
